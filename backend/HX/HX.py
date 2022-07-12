ae87397b8a332cd56bcc74873f2adab11095508b
Som Tambe <somvt@iitk.ac.in>
2022-07-13 00:48:19 +0530

    Revert to mcu code from random numbers

--- backend/HX/HX.py
+++ backend/HX/HX.py
@@ -6,7 +6,7 @@ import random
 import datetime
 os.system('modprobe w1-gpio') 
 os.system('modprobe w1-therm')
-# sensors = ["28-0301a279e726","28-0301a27991e5","28-0301a279e726","28-0301a279e726"]
+sensors = ["28-0301a279e726","28-0301a27991e5","28-0301a279e726","28-0301a279e726"]
 
 filename1 = "temp.csv"
 fields = ['T1', 'T2', 'T3', 'T4']
@@ -28,15 +28,15 @@ def csvWrite(val, fn):
 def startLogs(fn):
     temperature = [0, 0, 0, 0]
     while True:
-        # for x in range(len(sensors)):
-        #     filename = "/sys/bus/w1/devices/" + sensors[x] + "/w1_slave"
-        #     tempfile = open(filename) 
-        #     temptext = tempfile.read()
-        #     tempfile.close()
-        #     tempcelsius = temptext.split("\n")[1].split(" ")[9]
-        #     temper = float(tempcelsius[2:])
-        #     temperature[x] = temper/1000
-        temperature = [random.random() for _ in range(4)]
+        for x in range(len(sensors)):
+            filename = "/sys/bus/w1/devices/" + sensors[x] + "/w1_slave"
+            tempfile = open(filename) 
+            temptext = tempfile.read()
+            tempfile.close()
+            tempcelsius = temptext.split("\n")[1].split(" ")[9]
+            temper = float(tempcelsius[2:])
+            temperature[x] = temper/1000
+        # temperature = [random.random() for _ in range(4)]
 
         # print(temperature)
         csvWrite(temperature, fn)
